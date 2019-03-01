bf8469d1a5278dfb6380fdd474c83457f15d6ce2
thjennhjenoxi <sondtd00591@fpt.edu.vn>
2019-03-01 16:31:32 +0700

    up ListServices and Order

--- AspNetDemo/Areas/Admin/Controllers/DashboardController.cs
+++ AspNetDemo/Areas/Admin/Controllers/DashboardController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AspNetDemo.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,22 +9,48 @@ namespace AspNetDemo.Areas.Admin.Controllers
 {
     public class DashboardController : Controller
     {
+        ExcellOnServicesContext db = new ExcellOnServicesContext();
         // GET: Admin/Dashboard
         public ActionResult ListCustomer()
         {
             return View();
         }
+
         public ActionResult ListEmployee()
         {
             return View();
         }
+
         public ActionResult ListEmployeeActive()
         {
             return View();
         }
+
         public ActionResult ListEmployeeDeactive()
         {
             return View();
         }
+
+        public ActionResult ListServices()
+        {
+            return View(db.Services.ToList());
+        }
+
+        public ActionResult ListOrder()
+        {
+            return View(db.OrderServices.ToList());
+        }
+
+        public ActionResult ListOrder_Active()
+        {
+            return View();
+        }
+
+        public ActionResult ListOrder_Deactive()
+        {
+            return View();
+        }
+
+
     }
 }
