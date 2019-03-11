8144d20b8e52e8c555988b673f7ba34cabf77b9e
Tinh77 <tinhgiang9898@gmail.com>
2019-03-11 11:23:46 +0700

    Lay-du-lieu-tu-database-ra-view

--- AspNetDemo/Areas/Customer/Controllers/DashboardController.cs
+++ AspNetDemo/Areas/Customer/Controllers/DashboardController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using AspNetDemo.Models;
+using PagedList;
 
 namespace AspNetDemo.Areas.Customer.Controllers
 {
@@ -12,9 +13,15 @@ namespace AspNetDemo.Areas.Customer.Controllers
         ExcellOnServicesContext db = new ExcellOnServicesContext();
 
         // GET: Customer/Dashboard
-        public ActionResult Index()
+        public ActionResult Index(int? page)
         {
-            return View(db.Services.ToList());
+            if (Session["CompanyLogin"] == null)
+            {
+                return RedirectToAction("Login", "Login", new { Area = "Customer" });
+            }
+            var pageNumber = page ?? 1;
+            var pageSize = 5;
+            return View(db.OrderServices.OrderBy(x => x.Status == 2).OrderByDescending(x => x.CreatedAt).ToList().ToPagedList(pageNumber, pageSize));
         }
     }
 }
